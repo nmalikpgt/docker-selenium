0b044792367898b5aa1db95c1e54d11fc8e7811d
nmalikpgt <NMalik@pacificgeotech.com>
2024-04-24 15:51:47 -0700

    test

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,8 +1,17 @@
-echo "Checking if hub is ready"
-
+echo "Checking if hub is ready and time limit is 200 seconds"
+count=0
 while [ "$( curl -s http://10.10.10.215:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
 do
+  count=$((count+1))
+  echo "Attempt: ${count}"
+  if [ "$count" -ge 200 ]
+  then
+    echo "**** HUB IS NOT READY WITHIN 200 SECONDS ****"
+    exit 1
+    fi
 	sleep 1
 done
+echo "Selenium grid is up and running. Now Running Tests...."
+
 # start the java command
-java -cp selenium-docker.jar:selenium-docker-tests.jar:libs/* org.testng.TestNG Test.xml
+java -cp 'libs/*' org.testng.TestNG testng.xml
