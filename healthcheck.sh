e5471ead93e086b7f13364c19f8e0bc1618096d1
Nikhil Malik <112971301+nmalikpgt@users.noreply.github.com>
2024-04-24 14:11:09 -0700

    Update healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -5,9 +5,9 @@ while [ "$( curl -s http://10.10.10.215:4444/wd/hub/status | jq -r .value.ready 
 do
   count=$((count+1))
   echo "Attempt: ${count}"
-  if [ "$count" -ge 30 ]
+  if [ "$count" -ge 120 ]
   then
-    echo "**** HIB IS NOT READY WITHIN 30 SECONDS ****"
+    echo "**** HUB IS NOT READY WITHIN 120 SECONDS ****"
     exit 1
     fi
 	sleep 1
