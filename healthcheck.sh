86b8898dae819b6268acc5424ebaae5825faa5ce
Nikhil Malik <112971301+nmalikpgt@users.noreply.github.com>
2024-04-24 14:45:46 -0700

    Update healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,4 +1,4 @@
-echo "Checking if hub is ready"
+echo "Checking if hub is ready and time limit is 200 seconds"
 count=0
 while [ "$( curl -s http://10.10.10.215:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
 do
