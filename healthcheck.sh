639562e6bba3be8ff71e5ea7030aa8f2640f8376
nmalikpgt <NMalik@pacificgeotech.com>
2024-04-25 13:08:19 -0700

    commit changes

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,10 +1,10 @@
-echo "Checking if hub is ready and time limit is 200 seconds"
+echo "Checking if hub is ready and time limit is 60 seconds"
 count=0
 while [ "$( curl -s http://10.10.10.215:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
 do
   count=$((count+1))
   echo "Attempt: ${count}"
-  if [ "$count" -ge 200 ]
+  if [ "$count" -ge 60 ]
   then
     echo "**** HUB IS NOT READY WITHIN 200 SECONDS ****"
     exit 1
