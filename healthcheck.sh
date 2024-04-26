8f6d75e80364fe2f7607b37970d1a906aabb1bb8
nmalikpgt <NMalik@pacificgeotech.com>
2024-04-26 14:58:06 -0700

    rfgege

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,16 +1,16 @@
-#echo "Checking if hub is ready and time limit is 60 seconds"
-#count=0
-#while [ "$( curl -s http://10.10.10.215:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
-#do
-#  count=$((count+1))
-#  echo "Attempt: ${count}"
-#  if [ "$count" -ge 60 ]
-#  then
-#    echo "**** HUB IS NOT READY WITHIN 200 SECONDS ****"
-#    exit 1
-#    fi
-#	sleep 1
-#done
-#echo "Selenium grid is up and running. Now Running Tests...."
-# start the java command
+echo "Checking if hub is ready and time limit is 60 seconds"
+count=0
+while [ "$( curl -s http://10.10.10.215:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
+do
+  count=$((count+1))
+  echo "Attempt: ${count}"
+  if [ "$count" -ge 60 ]
+  then
+    echo "**** HUB IS NOT READY WITHIN 200 SECONDS ****"
+    exit 1
+    fi
+	sleep 1
+done
+echo "Selenium grid is up and running. Now Running Tests...."
+#start the java command
 java -cp 'libs/*' org.testng.TestNG testng.xml
