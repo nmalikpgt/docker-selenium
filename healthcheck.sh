940b45b1b8cc5a80cd0d9db482324e133ff2a94b
nmalikpgt <NMalik@pacificgeotech.com>
2024-04-29 15:04:52 -0700

    rthr

--- healthcheck.sh
+++ healthcheck.sh
@@ -4,7 +4,7 @@ while [ "$( curl -s http://10.10.10.215:4444/wd/hub/status | jq -r .value.ready 
 do
   count=$((count+1))
   echo "Attempt: ${count}"
-  if [ "$count" -ge 60 ]
+  if [ "$count" -ge 200 ]
   then
     echo "**** HUB IS NOT READY WITHIN 200 SECONDS ****"
     exit 1
