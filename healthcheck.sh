fba20075e679112a52aa105a0a3cdee4d22d3cba
nmalikpgt <NMalik@pacificgeotech.com>
2024-04-30 11:38:01 -0700

    rthr

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 'Checking if hub is ready and time limit is 60 seconds'
+echo 'Checking if hub is ready and time limit is 200 seconds'
 count=0
 while [ "$( curl -s http://10.10.10.215:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
 do
@@ -7,11 +7,11 @@ do
   echo "Attempt: ${count}"
   if [ "$count" -ge 200 ]
   then
-    echo "**** HUB IS NOT READY WITHIN 200 SECONDS ****"
-    exit 1
-  fi
-	sleep 1
-done
+        echo "**** HUB IS NOT READY WITHIN 200 SECONDS ****"
+        exit 1
+    fi
+    sleep 1
+  done
 
 echo "Selenium grid is up and running. Now Running Tests...."
 #start the java command
