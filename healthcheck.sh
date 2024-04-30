fb1d6b869169eb54db8ba1b1d5f37b9f8fa4b047
nmalikpgt <NMalik@pacificgeotech.com>
2024-04-30 14:43:53 -0700

    rthr

--- healthcheck.sh
+++ healthcheck.sh
@@ -8,11 +8,10 @@ do
   if [ "$count" -ge 200 ];
   then
         echo "**** HUB IS NOT READY WITHIN 200 SECONDS ****"
-        exit 1
-    fi
-    sleep 1
-  done
-
+        #exit 1
+  fi
+  sleep 1
+done
 echo "Selenium grid is up and running. Now Running Tests...."
 #start the java command
 java -cp 'libs/*' org.testng.TestNG testng.xml
