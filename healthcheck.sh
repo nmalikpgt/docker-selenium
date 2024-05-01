0093fd6cbc0e02d96affe047d70f27f063472770
nmalikpgt <NMalik@pacificgeotech.com>
2024-05-01 15:23:45 -0700

    regey

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,7 +2,7 @@ echo "Checking if hub is ready"
 
 while [ "$( curl -s http://10.10.10.215:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
 do
-	sleep 1
+	sleep 1;
 done
 # start the java command
 java -cp 'libs/*' org.testng.TestNG testng.xml
