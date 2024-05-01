6044f69a620c2cbf0db7928e20e2859f42b97f76
nmalikpgt <NMalik@pacificgeotech.com>
2024-05-01 11:26:45 -0700

    tghrt

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,21 +1,16 @@
-#!/bin/bash
-
 echo 'Checking if hub is ready and time limit is 200 seconds'
 count=0
-max_attempts=200  # Maximum number of attempts
-hub_url="http://10.10.10.215:4444/wd/hub/status"
-
-while [ "$(curl -s "$hub_url" | jq -r '.value.ready')" != "true" ];
-  do
-    ((count++))
-    echo "Attempt: $count"
-    if [ "$count" -ge "$max_attempts" ]; then
-        echo "**** HUB IS NOT READY WITHIN 200 SECONDS ****"
-        exit 1
-    fi
-    sleep 1
+while [ "$( curl -s http://10.10.10.215:4444/wd/hub/status | jq -r .value.ready )" != "true" ];
+do
+  count=$((count+1))
+  echo "Attempt: ${count}"
+  if [ "$count" -ge 200 ];
+  then
+      echo "**** HUB IS NOT READY WITHIN 200 SECONDS ****"
+      exit 1
+  fi
+  sleep 1
 done
-
 echo "Selenium grid is up and running. Now Running Tests...."
-# Start the java command
+#start the java command
 java -cp 'libs/*' org.testng.TestNG testng.xml
